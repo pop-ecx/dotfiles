a86125fb6e74905e193cdec84d84dbd549d5c5eb
pop-ecx <94111959+pop-ecx@users.noreply.github.com>
2024-04-30 13:17:44 +0300

    polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kilall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+MONITORS=$(xrandr --query | grep " connected" | cut -d "" -f1)
+MONITORS=$MONITORS polybar top
+
+echo "Bars launched..."
